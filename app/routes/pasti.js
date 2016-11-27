24173649ad9ae82be528f8f8c4dfdeb982232d04
Giovanni <giovanni.m.riva@gmail.com>
2016-11-27 16:45:35 +0100

    create routes

--- app/routes/pasti.js
+++ app/routes/pasti.js
@@ -1,26 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var express = require('express');
-var router = express.Router();
-
 /* GET request route */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 /* POST request route */
-router.post('/users/', function(req, res, next) {
+router.post('/pasti/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 /* PUT request route */
-router.put('/users/', function(req, res, next) {
+router.put('/pasti/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 /* DELETE request route */
-router.delete('/users/', function(req, res, next) {
+router.delete('/pasti/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
