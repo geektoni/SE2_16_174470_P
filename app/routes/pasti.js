72ed5d1317949c9d92693cf1d00daac97b14fb65
Giovanni <giovanni.m.riva@gmail.com>
2016-11-27 16:48:22 +0100

    Create controllers

--- app/routes/pasti.js
+++ app/routes/pasti.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET request route */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
 });
 
 /* POST request route */
 router.post('/pasti/', function(req, res, next) {
-  res.send('respond with a resource');
+
 });
 
 /* PUT request route */
 router.put('/pasti/', function(req, res, next) {
-  res.send('respond with a resource');
+
 });
 
 /* DELETE request route */
 router.delete('/pasti/', function(req, res, next) {
-  res.send('respond with a resource');
+
 });
 
 module.exports = router;
