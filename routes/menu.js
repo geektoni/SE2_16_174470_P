af53f3c847c8e48ff81c31723176e6ee6592b4e4
Giovanni <giovanni.m.riva@gmail.com>
2016-12-06 15:07:37 +0100

    improve menu routes

--- routes/menu.js
+++ routes/menu.js
@@ -5,13 +5,13 @@ var menuController = require('../controllers/menuController.js');
 //var ordiniController = require('../controllers/ordiniController.js');
 
 /* GET request route. */
-router.get('/settimana/:data', function(req, res, next) {    
+router.get('/settimana/:numero/', function(req, res, next) {
 	menuController.indexGiorni(req,res);
-	
+
 });
 
 /* GET request route. */
-router.get('/settimana/giorno/:data', function(req, res, next) {
+router.get('/settimana/:numero/giorno/:data', function(req, res, next) {
     menuController.indexPastiByGiorno(req,res);
 });
 module.exports = router;
