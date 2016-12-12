4c1405d6d7e701bfd11ec408746c30ea81655671
Giovanni <giovanni.m.riva@gmail.com>
2016-12-12 12:30:49 +0100

    implement show/hide of categorie rows

--- public/javascripts/ordini/ordine.js
+++ public/javascripts/ordini/ordine.js
@@ -1,5 +1,30 @@
 //JQuery
 
+//Display dei pasti per categoria
+var categorie_rows = $(".categoria_row");
+
+//Mostra solo la lista della prima categoria
+console.log(categorie_rows);
+categorie_rows.each(function (index,elem) {
+    if (index != 0){
+        $(elem).hide();
+    }
+});
+
+//Conferma scelta pasto
+$("#conferma .btn-primary").click(function () {
+    var next_elem;
+    categorie_rows.each(function (index,elem) {
+        if ($(elem).css('display') != 'none') {
+            $(elem).hide();
+            next_elem = index+1;
+        }
+    });
+    console.log(next_elem);
+    categorie_rows.eq(next_elem).show();
+});
+
+//Selezione del pasto
 var pasto_id;
 
 $(".card").mouseover(function () {
@@ -19,7 +44,4 @@ $("#informazioni .btn-primary").click(function () {
     location.href="/pasti/"+pasto_id;
 });
 
-//Conferma scelta pasto
-$("#conferma .btn-primary").click(function () {
 
-});
